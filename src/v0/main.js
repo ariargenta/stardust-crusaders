aaad2e65f71d16b37850d5f0b19c52b4a57281cd
Aria Argenta Silva Castañeda <55609849+ariargenta@users.noreply.github.com>
2025-08-25 09:33:35 -0600

    feat: initialise texture

--- src/v0/main.js
+++ src/v0/main.js
@@ -80,8 +80,52 @@ function initShaderProgram(gl, vertexShaderSource, fragmentShaderSource) {
     return shaderProgram;
 }
 
-function loadShader() {
-    
+function loadShader(gl, type, source) {
+    const shader = gl.createShader(type);
+
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+
+    if(!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        alert(`An error ocurred compiling the shaders: ${gl.getShaderInfoLog(shader)}`);
+
+        gl.deleteShader(shader);
+
+        return null;
+    }
+
+    return shader;
+}
+
+function initTexture(gl) {
+    const texture = gl.createTexture();
+    const internalFormat = gl.RGBA;
+    const width = 1;
+    const height = 1;
+    const border = 0;
+    const srcFormat = gl.RGBA;
+    const srcType = gl.UNSIGNED_BYTE;
+    const pixel = new Uint8Array([0, 0, 255, 255]);
+
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+
+    gl.texImage(
+        gl.TEXTURE_2D
+        , level
+        , internalFormat
+        , width
+        , height
+        , border
+        , srcFormat
+        , srcType
+        , pixel,
+    );
+
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+
+    return texture;
 }
 
 function main() {
@@ -115,6 +159,7 @@ function main() {
     };
 
     const vao = initBuffers(gl);
+    const texture = initTexture(gl);
     let then = 0;
 
     function render(time) {
